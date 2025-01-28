fe5b6c1c178071f6886893f971bb4037b7822485
Chaiyapop4092 <s6503051624092@email.kmutnb.ac.th>
2025-01-28 15:55:15 +0700

    เรียกรูปได้แล้ว

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,27 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:mytravel/constants/colors.dart';
+import 'package:mytravel/models/data.dart';
+import 'package:mytravel/widgets/destination.dart';
 
 class HomeScreen extends StatelessWidget {
  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          //row5 top destinations
-          Row(
-            children: [
-              Text("Top Destinations",style: TextStyle(fontSize: 25, color: primaryColors,fontWeight: FontWeight.w600),),
-              Spacer(),
-              Icon(Icons.tune,size: 25,color: primaryColors,),
-            ],
-          ),
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            //row5 top destinations
+            Row(
+              children: [
+                Text("Top Destinations",style: TextStyle(fontSize: 25, color: primaryColors,fontWeight: FontWeight.w600),),
+                Spacer(),
+                Icon(Icons.tune,size: 25,color: primaryColors,),
+              ],
+            ),
+            
+            
           
+          //row6 display images
+          GridView.count(crossAxisCount: 2,scrollDirection: Axis.vertical,shrinkWrap:true,childAspectRatio: 0.75, crossAxisSpacing:24,mainAxisSpacing: 24,children: List.generate(destinations.length, (index) {
+            var e = destinations[index];
 
-
-
-        ],
+            return AnimationConfiguration.staggeredGrid(position: index, columnCount: 2, child: SlideAnimation(
+              child: FadeInAnimation(
+                child: DestinationWidget(
+                  name: e.name,image: e.image,rate: e.rate,location: e.location,)),));
+          })),
+        
+        
+          ],
+        ),
       ),
     );
   }
