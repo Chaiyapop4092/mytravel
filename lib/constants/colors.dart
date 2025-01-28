f3abbd911ac9e6f6d2f4e763bc2565cba8ec1681
Chaiyapop4092 <s6503051624092@email.kmutnb.ac.th>
2025-01-28 14:42:11 +0700

    ก่อนพักเบรค

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const primaryColors = Color(0xff2b3848);
+const secondaryColors = Color.fromRGBO(169, 169, 169, 1);
+
+const forestColor = Color(0xffbee8af);
+const campingColor = Color(0xfff5da94);
+const boatColor = Color(0xffb3e4f1);
+
+
