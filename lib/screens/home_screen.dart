a846ccbd4d4ab511861334e0899b233103b1610f
Chaiyapop4092 <s6503051624092@email.kmutnb.ac.th>
2025-01-28 16:03:09 +0700

    ทำให้ scroll ได้

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,33 +11,40 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            //row5 top destinations
-            Row(
-              children: [
-                Text("Top Destinations",style: TextStyle(fontSize: 25, color: primaryColors,fontWeight: FontWeight.w600),),
-                Spacer(),
-                Icon(Icons.tune,size: 25,color: primaryColors,),
-              ],
-            ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              
+              
+              //row5 top destinations
+              const Row(
+                children: [
+                  Text("Top Destinations",style: TextStyle(fontSize: 25, color: primaryColors,fontWeight: FontWeight.w600),),
+                  Spacer(),
+                  Icon(Icons.tune,size: 25,color: primaryColors,),
+                ],
+              ),
+        
+              const SizedBox(height: 20,),
+              
+              
             
-            
+            //row6 display images
+            GridView.count(crossAxisCount: 2,scrollDirection: Axis.vertical,shrinkWrap:true,childAspectRatio: 0.75, crossAxisSpacing:24,mainAxisSpacing: 24,children: List.generate(destinations.length, (index) {
+              var e = destinations[index];
+        
+              return AnimationConfiguration.staggeredGrid(position: index, columnCount: 2, child: SlideAnimation(
+                child: FadeInAnimation(
+                  child: DestinationWidget(
+                    name: e.name,image: e.image,rate: e.rate,location: e.location,)),));
+            })),
           
-          //row6 display images
-          GridView.count(crossAxisCount: 2,scrollDirection: Axis.vertical,shrinkWrap:true,childAspectRatio: 0.75, crossAxisSpacing:24,mainAxisSpacing: 24,children: List.generate(destinations.length, (index) {
-            var e = destinations[index];
-
-            return AnimationConfiguration.staggeredGrid(position: index, columnCount: 2, child: SlideAnimation(
-              child: FadeInAnimation(
-                child: DestinationWidget(
-                  name: e.name,image: e.image,rate: e.rate,location: e.location,)),));
-          })),
-        
-        
-          ],
+          
+            ],
+          ),
         ),
       ),
     );
